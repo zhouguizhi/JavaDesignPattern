70f954d97fb09a006206e8065208e48ac8de5037
zhou.gz <2327322601@qq.com>
2021-02-19 23:57:50 +0800

    懒汉式的方式实现单例设计模式, 有线程安全和线程不安全的二种

---
+++ src/com/zgz/designpatterns/singleton/v3/Client.java
@@ -0,0 +1,7 @@
+package com.zgz.designpatterns.singleton.v3;
+
+public class Client {
+    public static void main(String[] args) {
+        Singleton instance = Singleton.getInstance();
+    }
+}
---
+++ src/com/zgz/designpatterns/singleton/v3/Singleton.java
@@ -0,0 +1,28 @@
+package com.zgz.designpatterns.singleton.v3;
+/**
+ * 懒汉式:实现单例
+ */
+public class Singleton {
+    private Singleton(){}
+    private static Singleton instance ;
+    //这个在多线程的环境下会出现问题
+//    public static Singleton getInstance(){
+//        //比如线程1进来了 判断了if条件为true,这个时候切换到线程2了,那么线程2也判断if条件为true,然后new了一个,然后线程1再new1 个对象,这样就有2个对象了,就违背了单例设计原则
+//        if(null==instance){
+//            instance = new Singleton();
+//        }
+//        return instance;
+//    }
+
+    /**
+     * 使用同步方法在多线程环境下保证创建的对象只有1个
+     * @return
+     */
+    public static synchronized Singleton getInstance(){
+        //比如线程1进来了 判断了if条件为true,这个时候切换到线程2了,那么线程2也判断if条件为true,然后new了一个,然后线程1再new1 个对象,这样就有2个对象了,就违背了单例设计原则
+        if(null==instance){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
