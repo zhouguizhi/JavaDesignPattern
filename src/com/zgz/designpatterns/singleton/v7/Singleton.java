a40de39ef089e432d6a9366f5cce9f1b7e021625
zhou.gz <2327322601@qq.com>
2021-02-20 11:13:33 +0800

    添加注释

--- src/com/zgz/designpatterns/singleton/v7/Singleton.java
+++ src/com/zgz/designpatterns/singleton/v7/Singleton.java
@@ -4,6 +4,7 @@ public class Singleton {
     private static Singleton INSTANCE = new Singleton();
     private static boolean isInit = false;
     private Singleton(){
+        //反射破坏单例的解决方案
         synchronized (Singleton.class){
             if(isInit){
                 throw new RuntimeException("不能通过反射创建多个对象");
