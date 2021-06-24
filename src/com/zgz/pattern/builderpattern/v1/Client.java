436567d5bd4108f7bd30e929d6cea8871f7bb30f
zhou.gz <2327322601@qq.com>
2021-06-24 20:10:52 +0800

    添加构造者模式

---
+++ src/com/zgz/pattern/builderpattern/v1/Client.java
@@ -0,0 +1,11 @@
+package com.zgz.pattern.builderpattern.v1;
+/**
+ * 定义一个电脑类,并给电脑类属性赋值,
+ * v1版本不要建造者设计模式看会有啥不好的地方
+ */
+public class Client {
+    public static void main(String[] args) {
+        Computer computer = new Computer();
+        computer.s
+    }
+}
